7feed03f7271a7abb04b549f6002c0408e70dd0e
Susan Ortiz <suu.ortizm@gmail.com>
2021-09-22 19:38:35 -0300

    se agrega barra de filtros y add

--- src/data.js
+++ src/data.js
@@ -1,8 +1,27 @@
-
-export const example = () => {
-  return "example";
+const dataOrder = {
+  //Ordenar de la A a la Z
+  orderAZ: function (printCharacters) {
+    let orderedResults = printCharacters.sort(function (a, b) {
+      if (a.name > b.name) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    console.log(1, orderedResults);
+    return orderedResults;
+  },
+  //Ordenar de la Z a la A
+  orderZA: function (printCharacters) {
+    let orderedResults = printCharacters.sort(function (a, b) {
+      if (a.name < b.name) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    console.log(2, orderedResults);
+    return orderedResults;
+  },
 };
-
-export const anotherExample = () => {
-  return "OMG";
-};
+export default dataOrder;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import data from "./data/rickandmorty/rickandmorty.js";
-//import { example } from "./data.js";
-
-//console.log(example, data);
+import dataOrder from "./data.js";
+//-------------Boton search---------------//
+console.log(dataOrder, data);
