fa6201603f076901ebdf7ec5e28f76b668f46d81
Susan Ortiz <suu.ortizm@gmail.com>
2021-09-23 13:04:15 -0300

    Se agregan cambios

--- src/data.js
+++ src/data.js
@@ -1,27 +1,4 @@
 const dataOrder = {
-  //Ordenar de la A a la Z
-  orderAZ: function (printCharacters) {
-    let orderedResults = printCharacters.sort(function (a, b) {
-      if (a.name > b.name) {
-        return 1;
-      } else {
-        return -1;
-      }
-    });
-    console.log(1, orderedResults);
-    return orderedResults;
-  },
-  //Ordenar de la Z a la A
-  orderZA: function (printCharacters) {
-    let orderedResults = printCharacters.sort(function (a, b) {
-      if (a.name < b.name) {
-        return 1;
-      } else {
-        return -1;
-      }
-    });
-    console.log(2, orderedResults);
-    return orderedResults;
-  },
+  searchCharacter: function (printCharacter) {},
 };
 export default dataOrder;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import data from "./data/rickandmorty/rickandmorty.js";
 import dataOrder from "./data.js";
+//traer datos de rickandmorty.js
+let printCharacter = data.results;
+const listCharacters = document.getElementById("characterCard");
+
+const searchBtn = document.getElementById("searchbtn");
+searchBtn.addEventListener("keyup", function (e) {
+  let searchValue = e.target.value;
+  searchBtn.value = searchValue;
+  console.log(searchValue);
+});
+
 //-------------Boton search---------------//
+
 console.log(dataOrder, data);
