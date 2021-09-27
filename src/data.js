2c70d8a5a7a0a55d6d9aa513a2ff6d09bc3a4c8e
Maferamosp <ramosmariaf@hotmail.com>
2021-09-27 16:18:04 -0300

    caja de filtros

--- src/data.js
+++ src/data.js
@@ -3,5 +3,17 @@ const dataOrder = {
     let searchName = printCharacter.filter((e) => e.name.includes(searchValue));
     return searchName;
   },
+  specieResults (printCharacter, species){
+    let onlySpecie = printCharacter.filter(printCharacter =>{
+      return printCharacter.species === species;
+    })
+    return onlySpecie;
+  },
+stateResults (printCharacter, state){
+  let onlyState = printCharacter.filter(printCharacter =>{
+    return printCharacter.state === state;
+  })
+  return onlyState;
+},
 };
 export default dataOrder;
