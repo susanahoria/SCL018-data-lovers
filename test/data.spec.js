27432a402ced975cd842d1028bae99fc0e04bbae
Susan Ortiz <suu.ortizm@gmail.com>
2021-09-29 13:05:46 -0300

    se agregan test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,31 +1,65 @@
-import {dataOrder} from '../src/data.js';
-let arrays = [
-  {name: "Rick Sanchaz", id: 1, species: "Human", status: "Alive" },
-  {name: "Morty Smith", id: 2, species: "Human", status: "Alive"},
-  {name: "Alexander", id: 12, species:"Human", status:"Dead"},
-  {name: "Alien Rick", id: 15, species: "Alien", status:"unknown"},
-  {name: "David Letterman", id: 91, species: "Human", status: "Alive"},
-  {name: "Doom-Nomitron", id: 104, species: "Alien", status: "Dead"}
-]
+import dataOrder from "../src/data.js";
+const array = [
+  { name: "Rick Sanchez", id: 1, species: "Human", status: "Alive" },
+  { name: "Morty Smith", id: 2, species: "Human", status: "Alive" },
+  { name: "Alexander", id: 12, species: "Human", status: "Dead" },
+  { name: "Alien Rick", id: 15, species: "Alien", status: "unknown" },
+  { name: "David Letterman", id: 91, species: "Human", status: "Alive" },
+  { name: "Doom-Nomitron", id: 104, species: "Alien", status: "Dead" },
+];
 
-describe('dataOrder', () => {
-  it('is a object', () => {
-    expect(typeof dataOrder).toBe('object');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-
-
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+describe("dataOrder", () => {
+  it("is a object", () => {
+    expect(typeof dataOrder).toBe("object");
   });
 });
+
+//test ordenar A a la Z
+describe("dataOrder.orderAZ", () => {
+  it("is a function", () => {
+    expect(typeof dataOrder.orderAZ).toBe("function");
+  });
+  it("should return order AZ", () => {
+    expect(dataOrder.orderAZ(array)).toEqual([
+      { name: "Alexander", id: 12, species: "Human", status: "Dead" },
+      { name: "Alien Rick", id: 15, species: "Alien", status: "unknown" },
+      { name: "David Letterman", id: 91, species: "Human", status: "Alive" },
+      { name: "Doom-Nomitron", id: 104, species: "Alien", status: "Dead" },
+      { name: "Morty Smith", id: 2, species: "Human", status: "Alive" },
+      { name: "Rick Sanchez", id: 1, species: "Human", status: "Alive" },
+    ]);
+  });
+});
+//test ordenar Z a la A
+describe("dataOrder.orderZA", () => {
+  it("is a function", () => {
+    expect(typeof dataOrder.orderZA).toBe("function");
+  });
+  it("should return order ZA", () => {
+    expect(dataOrder.orderZA(array)).toEqual([
+      { name: "Rick Sanchez", id: 1, species: "Human", status: "Alive" },
+      { name: "Morty Smith", id: 2, species: "Human", status: "Alive" },
+      { name: "Doom-Nomitron", id: 104, species: "Alien", status: "Dead" },
+      { name: "David Letterman", id: 91, species: "Human", status: "Alive" },
+      { name: "Alien Rick", id: 15, species: "Alien", status: "unknown" },
+      { name: "Alexander", id: 12, species: "Human", status: "Dead" },
+    ]);
+  });
+});
+//teest ordenar default
+describe("dataOrder.orderDefault", () => {
+  it("is a function", () => {
+    expect(typeof dataOrder.orderDefault).toBe("function");
+  });
+
+  it("should return order default", () => {
+    expect(dataOrder.orderDefault(array)).toEqual([
+      { name: "Rick Sanchez", id: 1, species: "Human", status: "Alive" },
+      { name: "Morty Smith", id: 2, species: "Human", status: "Alive" },
+      { name: "Alexander", id: 12, species: "Human", status: "Dead" },
+      { name: "Alien Rick", id: 15, species: "Alien", status: "unknown" },
+      { name: "David Letterman", id: 91, species: "Human", status: "Alive" },
+      { name: "Doom-Nomitron", id: 104, species: "Alien", status: "Dead" },
+    ]);
+  });
 });
